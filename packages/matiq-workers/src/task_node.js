9250cec40cf6c3cfae196c9ed9df2312edb522d8
hauld <1144493+hauld@users.noreply.github.com>
2023-03-31 22:29:27 +0200

    prevent memory leak in node worker

--- packages/matiq-workers/src/task_node.js
+++ packages/matiq-workers/src/task_node.js
@@ -4,40 +4,6 @@ const { parentPort } = require('worker_threads');
 const ivm = require('isolated-vm');
 parentPort.isolate = new ivm.Isolate({ memoryLimit: 128 });
 
-// Create a new context within this isolate. Each context has its own copy of all the builtin
-// Objects. So for instance if one context does Object.prototype.foo = 1 this would not affect any
-// other contexts.
-//const context = parentPort.isolate.createContextSync();
-
-// Get a Reference{} to the global object within the context.
-//const jail = context.global;
-
-// This makes the global object available in the context as `global`. We use `derefInto()` here
-// because otherwise `global` would actually be a Reference{} object in the new isolate.
-//jail.setSync('global', jail.derefInto());
-
-// We will create a basic `log` function for the new isolate to use.
-//jail.setSync('log', function(...args) {
-//	console.log(...args);
-//});
-
-// And let's test it out:
-//context.evalSync('log("hello world")');
-// > hello world
-
-
-/*
-const load_vm = async ()=>{
-  let pyodide_pkg = await import("./pyodide/pyodide.js");
-  //let url = new URL('pyodide', import.meta.url);
-  parentPort.pyodide = await pyodide_pkg.loadPyodide({
-    indexURL: 'pyodide'
-  });
-  await parentPort.pyodide.loadPackage(["numpy", "pytz"]);
-}
-
-let pyodideReadyPromise = load_vm();
-*/
 parentPort.onmessage = async (task) => {
   // make sure loading is done
   //await pyodideReadyPromise;
@@ -58,12 +24,9 @@ parentPort.onmessage = async (task) => {
 
     // Using the async version of `run` so that calls to `log` will get to the main node isolate
     let results = await hostile.run(context);//.catch(err => console.error(err));
-    // I've wasted 2MB
-    // I've wasted 4MB
-    // ...
-    // I've wasted 130MB
-    // I've wasted 132MB
-    // RangeError: Array buffer allocation failed
+
+    context.release();
+    
     parentPort.postMessage({ results });
   } catch (error) {
     parentPort.postMessage({ error: error.message });
